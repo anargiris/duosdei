2f57af76f7624b0bf9eeed144e08bca0b8982b33
anargiris <anargiriss@hotmail.com>
2021-12-14 00:48:33 +0200

    first

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          btn: "#debfff",
+          btnH: "#d2a7ff",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
