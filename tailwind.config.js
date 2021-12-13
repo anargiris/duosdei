90f3559f24d0627bdf1f751cdca9ec428e7db3c8
anargiris <anargiriss@hotmail.com>
2021-12-14 00:59:58 +0200

    dsda

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        Jura: ['"Jura"', "sans-serif"],
+      },
       colors: {
         purple: {
           btn: "#debfff",
